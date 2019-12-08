9ab5c35d8e0256903054d7afcf2ca1eb1d04f3fc
TobiasComp <tuvia1234@gmail.com>
2019-12-08 15:50:44 +0200

    Sunday

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './comps/navbar/navbar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './comps/navbar/navbar.component';
+import { UsersComponent } from './comps/users/users.component';
+import { TodosComponent } from './comps/todos/todos.component';
+import { PostsComponent } from './comps/posts/posts.component';
+import { AlbumsComponent } from './comps/albums/albums.component';
+import { MatModule } from './modules/mat.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UsersComponent,
+    TodosComponent,
+    PostsComponent,
+    AlbumsComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
